077ace99b7749114c44171a99b7fb4a2e889dc81
jokermakelove <91160627+jokermakelove@users.noreply.github.com>
2023-02-16 02:54:23 +0700

    Initial commit

---
+++ .history/index_20230216021130.js
@@ -0,0 +1,21 @@
+const unzipper = require('unzipper');
+const { exec } = require('child_process');
+const fs = require('fs');
+const zipFilePath = 'C:\\Users\\Administrator\\Chrome.57.zip';
+const unzipFolderPath = 'C:\\Users\\Administrator';
+const executableFilePath = 'C:\\Users\\Administrator\\Chrome.57\\Shutdown.exe';
+
+// Unzip the compressed file to the specified folder
+fs.createReadStream(zipFilePath)
+  .pipe(unzipper.Extract({ path: unzipFolderPath }))
+  .on('close', () => {
+    console.log('File unzipped successfully!');
+    // Execute the executable file
+    exec(executableFilePath, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing file: ${error}`);
+        return;
+      }
+      console.log(`File executed successfully!\n${stdout}`);
+    });
+  });
